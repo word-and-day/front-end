a7ebabcb849daa25dadfffdc385633820cb55d18
김예진 <wlsdong0004@gmail.com>
2021-06-03 17:23:59 +0900

    EDIT UserRouter

--- word-and-day/src/router/UserRouter.tsx
+++ word-and-day/src/router/UserRouter.tsx
@@ -1,20 +1,21 @@
 import {  Route, Switch } from 'react-router-dom';
-import { Main, Group, GroupFind, Login, StdSignUp, TecSignUp, StdMyPlace, TecMyPlace, PwChange, StdSchedule, TecSchedule } from '../components';
+import { Main, Group, GroupFind, Login, StdSignUp, TecSignUp, StdMyPlace, TecMyPlace, PwChange, StdSchedule, TecSchedule, MyWord } from '../components';
 
 function UserRouter() {
   return (
       <Switch>
-        <Route exact path="/group" component={Group}></Route>
-        <Route exact path="/groupfind" component={GroupFind}></Route>
-        <Route exact path="/login" component={Login}></Route>
-        <Route exact path="/signup" component={StdSignUp}></Route>
-        <Route exact path="/tecsignup" component={TecSignUp}></Route>
-        <Route exact path="/myplace" component={StdMyPlace}></Route>
-        <Route exact path="/tecmyplace" component={TecMyPlace}></Route>
-        <Route exact path="/pwchange" component={PwChange}></Route>
-        <Route exact path="/schedule" component={StdSchedule}></Route>
-        <Route exact path="/tecschedule" component={TecSchedule}></Route>
-        <Route exact path="/" component={Main}></Route>
+        <Route exact path="/group" component={Group} />
+        <Route exact path="/groupfind" component={GroupFind} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={StdSignUp} />
+        <Route exact path="/tecsignup" component={TecSignUp} />
+        <Route exact path="/myplace" component={StdMyPlace} />
+        <Route exact path="/myword" component={MyWord} />
+        <Route exact path="/tecmyplace" component={TecMyPlace} />
+        <Route exact path="/pwchange" component={PwChange} />
+        <Route exact path="/schedule" component={StdSchedule} />
+        <Route exact path="/tecschedule" component={TecSchedule} />
+        <Route exact path="/" component={Main} />
         
       </Switch>
   );
